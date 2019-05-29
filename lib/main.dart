131cb706d6cd1a59be88997225dc4ee846fdc8f0
Erin Morrissey <erinmorrissey@gmail.com>
2019-05-28 17:23:31 -0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    new MaterialApp(
+      title: "Hello Coffee",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Hello Coffee"),
+        ),
+      ),
+    ),
+  );
+}
